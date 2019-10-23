dc7f6aca4bc6b69e3770b44a74e43927eef66064
Wesley <fariawesley4@gmail.com>
2019-10-23 00:04:40 -0300

    DTID3-#3-PlayerPrototype [WIP]
    Starvation Bar, time rate fall, bar lookAt, status comunication with Player prototype done.

---
+++ Assets/Scripts/BarLookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarLookAt : MonoBehaviour
+{
+ 
+    //This will make the bar look at te camera.
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//This script will control the character.
+public class PlayerScript : MonoBehaviour
+{
+    //Starvation level controls if the character lever of starvation, 0 no starvation, 1 starvation, 2 extreme starvation.
+    [SerializeField]
+    private int starvationLevel = 0;
+    //Control death, false = alive, true = you are death... srry.
+    private bool death = false;
+
+    //Update bar situation to player
+    public void StatusUpdate(string barName, int status)
+    {
+        if (barName == "Starve")
+        {
+            starvationLevel = status;
+        }
+    }
+}
